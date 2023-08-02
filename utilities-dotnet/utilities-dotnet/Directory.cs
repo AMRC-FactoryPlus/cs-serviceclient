4ad01f18c5c02e1ab95def47eb5993356a1bd7de
Kallum Burgin <Kallb123@users.noreply.github.com>
2023-08-02 09:48:15 +0100

    Directory methods implemented

--- utilities-dotnet/utilities-dotnet/Directory.cs
+++ utilities-dotnet/utilities-dotnet/Directory.cs
@@ -1,6 +1,26 @@
 ﻿using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AMRC.FactoryPlus.ServiceClient;
+
+public struct ServiceProvider
+{
+    public Guid Device;
+    public string Url;
+
+    public ServiceProvider(Guid device, string url)
+    {
+        Device = device;
+        Url = url;
+    }
+}
+
+public struct ServiceProviderList
+{
+    public List<ServiceProvider> List;
+
+    public ServiceProviderList(List<ServiceProvider> list) => List = list;
+}
 
 /// <summary>
 /// The Directory service interface
@@ -20,12 +40,32 @@ public class Directory : ServiceInterface
     /// <returns>List of URLs</returns>
     public async UniTask<string[]> ServiceUrls(string service)
     {
-        // TODO: Fetch stuff
-        return new[] { "" };
+        var res = await Fetch($"/v1/service/{service}");
+
+        if (res.Status == 404)
+        {
+            Console.WriteLine($"{res.Status}: Can't find service {service}");
+            return new string[]{};
+        }
+
+        if (res.Status != 200)
+        {
+            throw new Exception($"{res.Status}: Can't get service records for {service}");
+        }
+
+        var specs = JsonConvert.DeserializeObject<ServiceProviderList>(res.Content);
+        return specs.List.Select(s => s.Url).Where(s => !String.IsNullOrEmpty(s)).ToArray();
     }
 
-    public void RegisterServiceUrl(string service, string url)
+    public async void RegisterServiceUrl(string service, string url)
     {
-        // TODO: PUT stuff
+        var res = await Fetch($"/v1/service/{service}/advertisment", "PUT", null, null, $"{{\"url\": \"{url}\"}}");
+        
+        if (res.Status != 204)
+        {
+            throw new Exception($"{res.Status}: Can't register service {service}");
+        }
+
+        Console.WriteLine($"Registered {url} for {service}");
     }
 }
