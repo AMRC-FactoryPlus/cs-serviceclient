1070ade4f813380320b3009bf5071824d268acfa
Kallum Burgin <Kallb123@users.noreply.github.com>
2023-08-09 14:59:19 +0100

    Begin to interact with MQTT library

--- utility-sample/utility-sample/MVVM/ViewModel/HomeViewModel.cs
+++ utility-sample/utility-sample/MVVM/ViewModel/HomeViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using AMRC.FactoryPlus;
+﻿using System.Diagnostics;
 using AMRC.FactoryPlus.ServiceClient;
+using Com.Cirruslink.Sparkplug.Protobuf;
+using MQTTnet;
+using MQTTnet.Client;
 using utility_sample.Core;
 using utility_sample.MVVM.Model;
 
@@ -13,12 +13,12 @@ namespace utility_sample.MVVM.ViewModel
     /// </summary>
     public class HomeViewModel: ObservableObject
     {
-        /// <summary>
-        /// TODO: implement command
-        /// </summary>
         public RelayCommand SubmitCommand { get; set; }
+        public RelayCommand CancelCommand { get; set; }
 
         private FPlusCommunicator _fPlusCommunicator;
+        private ServiceClient _serviceClient;
+        private IMqttClient _mqttClient;
         
         /// <summary>
         /// Constructor for making an instance of the settings view model
@@ -29,9 +29,47 @@ namespace utility_sample.MVVM.ViewModel
             
             SubmitCommand = new RelayCommand(o =>
             {
-                // TODO: Submit F+ Request
-                MessageBox.Show(_fPlusCommunicator.TestString);
+                StartFPlusStuff();
             });
+            
+            CancelCommand = new RelayCommand(o =>
+            {
+                StopFPlusStuff();
+            });
+        }
+        
+        private async void StartFPlusStuff()
+        {
+            _serviceClient = new ServiceClient();
+            // var test = await serviceClient.ConfigDb.Search(UUIDs.App[AppSubcomponents.Info], new Dictionary<string, object>(), new Dictionary<string, string>(), null);
+            
+            // _mqttClient = await serviceClient.Mqtt.GetMqttClient("mqtt://amrcf2050mqtt.shef.ac.uk:1883");
+            _mqttClient = await _serviceClient.Mqtt.GetMqttClient();
+
+            _serviceClient.Mqtt.OnMessageReceived += MessageReceived;
+
+            // "spBv1.0/AMRC-F2050-IoT_Power_Monitoring/DDATA/Soft_Gateway/F2050_Mk1_Canteen_Coffee_Machine"
+            // "rng/sample_1"
+            // "pathfindr/dog/summary/latest"
+            await _serviceClient.Mqtt.SubscribeToTopic(_mqttClient, "spBv1.0/#");
+            // MessageBox.Show(test.Length.ToString());
+        }
+
+        private void MessageReceived(IMqttClient mqttClient, Payload payload, MqttApplicationMessage mqttMessageObject)
+        {
+            if (mqttClient != _mqttClient)
+            {
+                return;
+            }
+            Debug.WriteLine($"Message received on: {mqttMessageObject.Topic}");
+            Debug.WriteLine(payload.Metrics.Count);
+            Debug.WriteLine(payload.Metrics);
+        }
+
+        private async void StopFPlusStuff()
+        {
+            await _serviceClient.Mqtt.UnsubscribeFromTopic(_mqttClient, "spBv1.0/#");
+            await _mqttClient.DisconnectAsync();
         }
     }
 }
