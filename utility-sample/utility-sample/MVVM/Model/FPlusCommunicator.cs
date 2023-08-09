06c2923270551bd34a8eaeede95105c282694e4a
Rhys Blackett <rhysblackett@live.co.uk>
2023-08-09 16:23:20 +0100

    Merge fixes

--- utility-sample/utility-sample/MVVM/Model/FPlusCommunicator.cs
+++ utility-sample/utility-sample/MVVM/Model/FPlusCommunicator.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using AMRC.FactoryPlus.ServiceClient;
+using Com.Cirruslink.Sparkplug.Protobuf;
+using MQTTnet;
+using MQTTnet.Client;
 
 namespace utility_sample.MVVM.Model
 {
@@ -21,6 +25,7 @@ namespace utility_sample.MVVM.Model
         private string _mqttUrl;
 
         private ServiceClient _serviceClient;
+        private IMqttClient _mqttClient;
 
         /// <summary>
         /// Gets the current instance of this class
@@ -49,12 +54,34 @@ namespace utility_sample.MVVM.Model
             _mqttUrl = ConfigurationManager.AppSettings.Get("MqttUrl") ?? string.Empty;
         }
 
-        /// <summary>
-        /// TODO
-        /// </summary>
-        public async void DoTest(string username, string password)
+        public async void StartFPlusStuff(string username, string password)
         {
-            var test = await _serviceClient.ConfigDb.Search(UUIDs.App[AppSubcomponents.Info], new Dictionary<string, object>(), new Dictionary<string, string>(), null);
+            _serviceClient = new ServiceClient();
+            // var test = await serviceClient.ConfigDb.Search(UUIDs.App[AppSubcomponents.Info], new Dictionary<string, object>(), new Dictionary<string, string>(), null);
+            
+            _mqttClient = await _serviceClient.Mqtt.GetMqttClient();
+
+            _serviceClient.Mqtt.OnMessageReceived += MessageReceived;
+
+            await _mqttClient.SubscribeAsync("spBv1.0/#");
+            // MessageBox.Show(test.Length.ToString());
+        }
+        
+        public async void StopFPlusStuff()
+        {
+            await _mqttClient.UnsubscribeAsync("spBv1.0/#");
+            await _mqttClient.DisconnectAsync();
+        }
+
+        private void MessageReceived(IMqttClient mqttClient, Payload payload, MqttApplicationMessage mqttMessageObject)
+        {
+            if (mqttClient != _mqttClient)
+            {
+                return;
+            }
+            Debug.WriteLine($"Message received on: {mqttMessageObject.Topic}");
+            Debug.WriteLine(payload.Metrics.Count);
+            Debug.WriteLine(payload.Metrics);
         }
     }
 }
--- utility-sample/utility-sample/MVVM/ViewModel/HomeViewModel.cs
+++ utility-sample/utility-sample/MVVM/ViewModel/HomeViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using System.Diagnostics;
-using AMRC.FactoryPlus.ServiceClient;
-using Com.Cirruslink.Sparkplug.Protobuf;
-using MQTTnet;
-using MQTTnet.Client;
 using utility_sample.Core;
 using utility_sample.MVVM.Model;
 
@@ -20,8 +16,6 @@ namespace utility_sample.MVVM.ViewModel
         public RelayCommand CancelCommand { get; set; }
 
         private FPlusCommunicator _fPlusCommunicator;
-        private ServiceClient _serviceClient;
-        private IMqttClient _mqttClient;
         
         /// <summary>
         /// Constructor for making an instance of the settings view model
@@ -34,45 +28,14 @@ namespace utility_sample.MVVM.ViewModel
             {
                 if (ServiceUsername != null && ServicePassword != null)
                 {
-                    _fPlusCommunicator.DoTest(ServiceUsername, ServicePassword);
+                    _fPlusCommunicator.StartFPlusStuff(ServiceUsername, ServicePassword);
                 }
-                StartFPlusStuff();
             });
             
             CancelCommand = new RelayCommand(o =>
             {
-                StopFPlusStuff();
+                _fPlusCommunicator.StopFPlusStuff();
             });
-        }
-        
-        private async void StartFPlusStuff()
-        {
-            _serviceClient = new ServiceClient();
-            // var test = await serviceClient.ConfigDb.Search(UUIDs.App[AppSubcomponents.Info], new Dictionary<string, object>(), new Dictionary<string, string>(), null);
-            
-            _mqttClient = await _serviceClient.Mqtt.GetMqttClient();
-
-            _serviceClient.Mqtt.OnMessageReceived += MessageReceived;
-
-            await _mqttClient.SubscribeAsync("spBv1.0/#");
-            // MessageBox.Show(test.Length.ToString());
-        }
-
-        private void MessageReceived(IMqttClient mqttClient, Payload payload, MqttApplicationMessage mqttMessageObject)
-        {
-            if (mqttClient != _mqttClient)
-            {
-                return;
-            }
-            Debug.WriteLine($"Message received on: {mqttMessageObject.Topic}");
-            Debug.WriteLine(payload.Metrics.Count);
-            Debug.WriteLine(payload.Metrics);
-        }
-
-        private async void StopFPlusStuff()
-        {
-            await _mqttClient.UnsubscribeAsync("spBv1.0/#");
-            await _mqttClient.DisconnectAsync();
         }
     }
 }
