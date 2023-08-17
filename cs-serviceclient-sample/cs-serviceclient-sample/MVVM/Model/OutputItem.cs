d40eca4ea0c3a4b204377eb2479a75afc861ae20
Rhys Blackett <rhysblackett@live.co.uk>
2023-08-17 13:18:02 +0100

    Logic for output view model and view

---
+++ cs-serviceclient-sample/cs-serviceclient-sample/MVVM/Model/OutputItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace utility_sample.MVVM.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OutputItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Content { get; set; }
+
+        public OutputItem(string content, DateTime? dateTime = null)
+        {
+            Time = dateTime ?? DateTime.Now;
+            Content = content;
+        }
+    }
+}
--- cs-serviceclient-sample/cs-serviceclient-sample/MVVM/ViewModel/OutputViewModel.cs
+++ cs-serviceclient-sample/cs-serviceclient-sample/MVVM/ViewModel/OutputViewModel.cs
@@ -1,14 +1,23 @@
-﻿using utility_sample.Core;
+﻿using System.Collections.Generic;
+using utility_sample.Core;
+using utility_sample.MVVM.Model;
 
 namespace utility_sample.MVVM.ViewModel
 {
-    
+    /// <summary>
+    /// 
+    /// </summary>
     public class OutputViewModel : ObservableObject
     {
+        public List<OutputItem> OutputItems { get; set; }
         
         public OutputViewModel()
         {
-            
+        }
+
+        private void AddItem(OutputItem outputItem)
+        {
+            OutputItems.Add(outputItem);
         }
     }
 }
